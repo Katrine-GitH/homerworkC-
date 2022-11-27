18563348035ee0ab1c6aea77c9f1b4d902da1575
Kate <tsvetkova.e.i@mail.ru>
2022-11-28 00:51:05 +0300

    1version

---
+++ 3Problem/Evennumber.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int i =2;
+int N =5;
+int f =1;
+if (i<=N) {{
+    if (i/2==0){
+    Console.WriteLine ("f=f,i");
+    }
+    }
+    i=i+1;
+}
+Console.WriteLine ("f",f);
