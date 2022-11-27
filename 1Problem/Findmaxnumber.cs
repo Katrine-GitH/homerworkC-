8c588b9add943ff8909945b7d517498e429174a9
Kate <tsvetkova.e.i@mail.ru>
2022-11-27 22:35:11 +0300

    Findmaxnumber

---
+++ 1Problem/Findmaxnumber.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a = 2;
+int b = 3;
+int c = 7;
+int max = 0;
+if (a > b) {
+    if (a <=c)
+    {
+        max = a;
+    }
+    max = c; 
+} else {
+        if (b <c)
+        {
+            max = b;    
+        }
+        max = c;
+}  
+Console.WriteLine("max: " + max);
