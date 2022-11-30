8b7c41ed03a1b3efd13b8819edf20f767a6e7b61
Kate <tsvetkova.e.i@mail.ru>
2022-11-30 14:27:36 +0300

    version2

--- 3Problem/Evennumber.cs
+++ 3Problem/Evennumber.cs
@@ -4,25 +4,15 @@ int count =5;
 List<int> evenNumbers = new List<int>(); 
 int index =0;
 
-Console.WriteLine ("My");
-for(int i = 0; i<count; i++){
-   Console.WriteLine (numbers[i]); 
-}
-
-Console.WriteLine ("Your");
 while (index<count) {
 
     if (numbers[index]%2==0) {
-        Console.WriteLine ($"numbers[{index}]={numbers[index]}");
        // Console.WriteLine (numbers[index]);     
        evenNumbers.Add(numbers[index]);
     }
 
     index = index+1;
-    Console.WriteLine($"index={index}");  
-}
-Console.WriteLine ($"evenNumbers={evenNumbers.ToString()}");
-
+    }
 foreach (int c in evenNumbers)
 {
     Console.WriteLine(c);
