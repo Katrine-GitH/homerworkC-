2e302b6a06e96052a041cf4baabcd80924a0852d
Kate <tsvetkova.e.i@mail.ru>
2022-11-30 13:02:28 +0300

    version1

--- 3Problem/Evennumber.cs
+++ 3Problem/Evennumber.cs
@@ -1,12 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-int i =2;
-int N =5;
-int f =1;
-if (i<=N) {{
-    if (i/2==0){
-    Console.WriteLine ("f=f,i");
-    }
-    }
-    i=i+1;
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+int[] numbers = new int[5] {1,2,3,4,5};
+int count =5;
+List<int> evenNumbers = new List<int>(); 
+int index =0;
+
+Console.WriteLine ("My");
+for(int i = 0; i<count; i++){
+   Console.WriteLine (numbers[i]); 
 }
-Console.WriteLine ("f",f);
+
+Console.WriteLine ("Your");
+while (index<count) {
+
+    if (numbers[index]%2==0) {
+        Console.WriteLine ($"numbers[{index}]={numbers[index]}");
+       // Console.WriteLine (numbers[index]);     
+       evenNumbers.Add(numbers[index]);
+    }
+
+    index = index+1;
+    Console.WriteLine($"index={index}");  
+}
+Console.WriteLine ($"evenNumbers={evenNumbers.ToString()}");
+
+foreach (int c in evenNumbers)
+{
+    Console.WriteLine(c);
+}
